f29adedcb49de937d2918128b9c6701e6ac38866
Petasco <peterdiyouh@gmail.com>
2022-08-01 21:38:12 +0100

    This game is to determine how good or lucky a user is, in guessing a number from 1 to 10

---
+++ Petasco_GuessingGame_Meter.py
@@ -0,0 +1,39 @@
+''' this is a guessing game that determine how or lucky a user is, in guessing a number from 1 to 10'''
+
+import random
+print("---------------------------------------------------------")
+print("~~~~~~~ WELCOME TO PETASCO GUESSING GAME ~~~~~~~~~~~~~~~~")
+print("---------------------------------------------------------")
+
+random_num = random.randint(1, 11)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("MAKE A GUESS FROM 1 TO 10: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    #if int(user_guess) <= int(0) and int(user_guess) >= int(11):
+       # print("<=`,`,`,`,`,`,`,`,`,`,`,`,=> OOPs! ENTER A NUMBER FROM 1 TO 10 <=`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,=>")
+        #quit()
+
+    # end the program if the user enter a string
+    else:
+        print("<=`,`,`,`,`,`,`,`,`,`,`,`,=> OOPs! ENTER A NUMBER NEXT TIME! <=`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,=>")
+        continue
+    if user_guess == random_num:
+        print("CONGRATS, YOU GOT IT")
+        break
+    else:
+        print("<=`,`,`,`,`,`,`,`,`,`,`,`,=> OOPs! YOU GOT IT WRONG! <=`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,=>")
+        # the line below is hack the game for the random number
+        #print("THE RANDOM NUMBER WAS:", random_num)
+
+    if guesses == 10:
+        print("<=`,`,`,`,`,`,`,`,`,`,`,`,=> OOPs! GAME OVER!!! START AGAIN!!! <=`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,=>")
+        quit()
+
+print("YOU GOT IT IN", guesses, "GUESSES OUT OF 10")
+loss_in_percentage = (guesses/10) * 100
+won_in_percentage = 100 - float(loss_in_percentage)
+print("YOU ARE " + str(won_in_percentage) + "% GOOD IN GUESSING!")
